60dbba197e6ddf89f677e00d27e70a0cce8706fb
Darling Sánchez <darlingprogramador47@gmail.com>
2021-10-03 20:29:41 -0600

    Creacion de theme WordPress

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+<main class="container">
+    <div class="pagina-404">
+        <h1>404 PAGINA NO ENCONTRADA</h1>
+        <h2>Haga <a href="<?php echo home_url() ?>">click aquí</a> para volver a la página principal</h2>
+    </div>
+</main>
+<?php get_footer() ?>
